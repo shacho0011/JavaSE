85ceead6b1ec32705610932635b1b54b3bea25b4
shacho0011 <shacho0011@gmail.com>
2016-07-25 12:27:20 +0600

    new work

--- BloodBankManagementSystem/src/Main.java
+++ BloodBankManagementSystem/src/Main.java
@@ -2,13 +2,18 @@ import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 
-public class Main {
+public class Main extends JFrame{
 	public static void main(String args[]){
+		Menu menu = new Menu();
 		
+		menu.setVisible(true);
+		menu.setSize(800,600);
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/*
 		DonorRegistrationForm donor = new DonorRegistrationForm();
 		donor.setVisible(true);
-		//donor.setLayout(null);
 		donor.setSize(800,600);
 		donor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		*/
 	}
 }
